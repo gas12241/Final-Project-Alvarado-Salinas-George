95dd1e7d394fab9394e094618c48ce7e17fa1c58
Yuanyuan Zhao <qsn7pu@virginia.edu>
2023-03-27 21:03:25 -0400

    finished tshirt_api initally

--- src/main/java/com/company/gamestore/controller/TshirtController.java
+++ src/main/java/com/company/gamestore/controller/TshirtController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -13,23 +14,26 @@ public class TshirtController {
     @Autowired
     TshirtRepository tshirtRepository;
     @GetMapping(path = "/tshirt/{color}")
+    @ResponseStatus(value = HttpStatus.OK)
     public List<Tshirt> getTshirtByColor(@PathVariable String color) {
         return tshirtRepository.findByColor();
     }
     @GetMapping(path = "/tshirt/{size}")
+    @ResponseStatus(value = HttpStatus.OK)
     public List<Tshirt> getTshirtBySize(@PathVariable String size) {
         return tshirtRepository.findBySize();
     }
     @GetMapping(path = "/tshirt")
+    @ResponseStatus(value = HttpStatus.OK)
     public List<Tshirt> getTshirts() {
         List<Tshirt> tshirts =  tshirtRepository.findAll();
         return tshirts;
     }
 
     @PostMapping(path = "/tshirt")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void createTshirt(@RequestBody Tshirt tshirt) {
-        tshirtRepository.save(tshirt);
+    @ResponseStatus(HttpStatus.CREATED)
+    public Tshirt createTshirt(@RequestBody @Valid Tshirt tshirt) {
+        return tshirtRepository.save(tshirt);
     }
 
     @DeleteMapping(path = "/tshirt/{id}")
@@ -38,6 +42,7 @@ public class TshirtController {
         tshirtRepository.deleteById(id);
     }
     @PutMapping(path = "/tshirt")
+    @ResponseStatus(value = HttpStatus.CREATED)
     public void updateTshirt(@RequestBody Tshirt tshirt) {
         tshirtRepository.save(tshirt);
     }
