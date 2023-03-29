76e20f5ed1e0e21f6e87a8a0b636879354258776
Yuanyuan Zhao <qsn7pu@virginia.edu>
2023-03-29 18:43:09 -0400

    added mockmvc tests for tshirt

---
+++ src/test/java/com/company/gamestore/controller/TshirtControllerTest.java
@@ -0,0 +1,91 @@
+package com.company.gamestore.controller;
+
+import com.company.gamestore.model.Tshirt;
+import com.company.gamestore.repository.TshirtRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.junit.Assert.*;
+@RunWith(SpringRunner.class)
+@WebMvcTest(TshirtController.class)
+public class TshirtControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private TshirtRepository tshirtRepository;
+    private ObjectMapper mapper = new ObjectMapper();
+    @Test
+    public void getTshirtByColor() throws Exception {
+
+        mockMvc.perform(get("/tshirt/green"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getTshirtBySize() throws Exception {
+        mockMvc.perform(get("/tshirt/large"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+    @Test
+    public void getTshirts() throws Exception {
+        mockMvc.perform(get("/tshirt"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void createTshirt() throws Exception {
+        Tshirt tshirt = new Tshirt();
+        tshirt.setColor("green");
+        tshirt.setDescription("nike t-shirt for children");
+        tshirt.setPrice(12);
+        tshirt.setSize("large");
+        tshirt.setQuantity(100);
+        tshirt.setTshirtId(1);
+        String inputJson = mapper.writeValueAsString(tshirt);
+
+        mockMvc.perform(post("/tshirt").content(inputJson).contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void deleteTshirt() throws Exception{
+        mockMvc.perform(delete("/tshirt/1"))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void updateTshirt() throws Exception{
+        Tshirt tshirt = new Tshirt();
+        tshirt.setColor("green");
+        tshirt.setDescription("nike t-shirt for children");
+        tshirt.setPrice(12);
+        tshirt.setSize("large");
+        tshirt.setQuantity(100);
+        tshirt.setTshirtId(1);
+        String inputJson = mapper.writeValueAsString(tshirt);
+
+        mockMvc.perform(put("tshirt").contentType(MediaType.APPLICATION_JSON).content(inputJson))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+    }
+}
---
+++ src/test/java/com/company/gamestore/repository/TshirtRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.company.gamestore.repository;
+
+import static org.junit.Assert.*;
+
+public class TshirtRepositoryTest {
+    // Create
+    // Read
+    // Read All
+    // Update
+    // Delete
+    // By Color
+    // By Size
+
+}
