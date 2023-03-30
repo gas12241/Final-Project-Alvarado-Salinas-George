13fd28229c43c377e5f026b3680e226be9a12708
Dickson Afful <fiifi.dev@gmail.com>
2023-03-29 20:04:08 -0400

    fix: game

--- src/main/java/com/company/gamestore/controller/InvoiceController.java
+++ src/main/java/com/company/gamestore/controller/InvoiceController.java
@@ -1,2 +1,30 @@
-package com.company.gamestore.controller;public class InvoiceController {
+package com.company.gamestore.controller;
+
+import com.company.gamestore.model.Invoice;
+import com.company.gamestore.repository.InvoiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController("/invoices")
+public class InvoiceController {
+    @Autowired
+    InvoiceRepository invoiceRepository;
+
+
+    @GetMapping(path = "")
+    @ResponseStatus(value = HttpStatus.OK)
+    public List<Invoice> getAllInvoices() {
+        return invoiceRepository.findAll();
+    }
+
+    @GetMapping(path = "")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public List<Invoice> createInvoice() {
+        return invoiceRepository.findAll();
+    }
 }
