76581ef8f36589c6bc0cc317524984eec435aabd
Yuanyuan Zhao <qsn7pu@virginia.edu>
2023-03-31 19:30:03 -0400

    fixed tshirt tests

--- src/test/java/com/company/gamestore/repository/TshirtRepositoryTest.java
+++ src/test/java/com/company/gamestore/repository/TshirtRepositoryTest.java
@@ -36,12 +36,12 @@ public class TshirtRepositoryTest {
         Tshirt tshirt = new Tshirt();
         tshirt.setColor("green");
         tshirt.setDescription("nike t-shirt for children");
-        tshirt.setPrice(BigDecimal.valueOf(12));
+        tshirt.setPrice(BigDecimal.valueOf(12L, 2));
         tshirt.setSize("large");
         tshirt.setQuantity(100);
         tshirt = tshirtRepository.save(tshirt);
 
-        List<Tshirt> tshirt1 = tshirtRepository.findBySize("green");
+        List<Tshirt> tshirt1 = tshirtRepository.findByColor("green");
         assertEquals(tshirt1.get(0), tshirt);
     }
     @Test
@@ -49,7 +49,7 @@ public class TshirtRepositoryTest {
         Tshirt tshirt = new Tshirt();
         tshirt.setColor("green");
         tshirt.setDescription("nike t-shirt for children");
-        tshirt.setPrice(BigDecimal.valueOf(12));
+        tshirt.setPrice(BigDecimal.valueOf(12L, 2));
         tshirt.setSize("large");
         tshirt.setQuantity(100);
         tshirt = tshirtRepository.save(tshirt);
@@ -63,7 +63,7 @@ public class TshirtRepositoryTest {
         Tshirt tshirt1 = new Tshirt();
         tshirt1.setColor("green");
         tshirt1.setDescription("nike t-shirt for children");
-        tshirt1.setPrice(BigDecimal.valueOf(12));
+        tshirt1.setPrice(BigDecimal.valueOf(12L, 2));
         tshirt1.setSize("large");
         tshirt1.setQuantity(100);
         tshirt1 = tshirtRepository.save(tshirt1);
@@ -84,7 +84,7 @@ public class TshirtRepositoryTest {
         Tshirt tshirt = new Tshirt();
         tshirt.setColor("green");
         tshirt.setDescription("nike t-shirt for children");
-        tshirt.setPrice(BigDecimal.valueOf(12));
+        tshirt.setPrice(BigDecimal.valueOf(12L, 2));
         tshirt.setSize("large");
         tshirt.setQuantity(100);
         tshirt = tshirtRepository.save(tshirt);
@@ -98,7 +98,7 @@ public class TshirtRepositoryTest {
         Tshirt tshirt = new Tshirt();
         tshirt.setColor("green");
         tshirt.setDescription("nike t-shirt for children");
-        tshirt.setPrice(BigDecimal.valueOf(12));
+        tshirt.setPrice(BigDecimal.valueOf(12L, 2));
         tshirt.setSize("large");
         tshirt.setQuantity(100);
         tshirt = tshirtRepository.save(tshirt);
@@ -115,7 +115,7 @@ public class TshirtRepositoryTest {
         Tshirt tshirt = new Tshirt();
         tshirt.setColor("green");
         tshirt.setDescription("nike t-shirt for children");
-        tshirt.setPrice(BigDecimal.valueOf(12));
+        tshirt.setPrice(BigDecimal.valueOf(12L, 2));
         tshirt.setSize("large");
         tshirt.setQuantity(100);
         tshirt = tshirtRepository.save(tshirt);
@@ -124,9 +124,4 @@ public class TshirtRepositoryTest {
         Optional<Tshirt> tshirt1 = tshirtRepository.findById(tshirt.getTshirtId());
         assertFalse(tshirt1.isPresent());
     }
-
-
-
-
-
 }
